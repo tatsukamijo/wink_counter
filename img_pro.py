b94150054379df340b26ed77ab10f8555c69c3d2
tatsukamijo <tatsukamijo@icloud.com>
2022-07-05 15:50:13 +0900

    count

--- img_pro.py
+++ img_pro.py
@@ -1,22 +1,25 @@
 import os,sys
 import cv2
+from sqlalchemy import false, true
 
 cap = cv2.VideoCapture(1)
 cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye_tree_eyeglasses.xml')
 
 count = 0
+temp_side = false
+temp_both = false
 
 while True:
     ret, rgb = cap.read()
-
+    
     gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
     faces = cascade.detectMultiScale(
         gray, scaleFactor=1.11, minNeighbors=3, minSize=(100, 100))
 
     if len(faces) == 1:
         x, y, w, h = faces[0, :]
-        cv2.rectangle(rgb, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # cv2.rectangle(rgb, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         # 処理高速化のために顔の上半分を検出対象範囲とする
         eyes_gray = gray[y : y + int(h/2), x : x + w]
@@ -26,10 +29,23 @@ while True:
         for ex, ey, ew, eh in eyes:
             cv2.rectangle(rgb, (x + ex, y + ey), (x + ex + ew, y + ey + eh), (255, 255, 0), 1)
 
+        if len(eyes) == 2:
+            temp_both = true
+            temp_side = false
+
         if len(eyes) == 1:
-            count += 1
-            cv2.putText(rgb,"%d" %(count),
-                (10,100), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 2, cv2.LINE_AA)
+            if temp_both == true:
+                count += 1
+
+            temp_both = false
+            temp_side = true
+        
+        if count == 10:
+            cv2.putText(rgb, 'reached 10!',
+                (500,50), cv2.FONT_HERSHEY_PLAIN, 3, (0,100,255), 2, cv2.LINE_AA)
+
+    cv2.putText(rgb, f'wink count {count}',
+                (500,100), cv2.FONT_HERSHEY_PLAIN, 3, (0,100,255), 2, cv2.LINE_AA)
 
     cv2.imshow('frame', rgb)
     if cv2.waitKey(1) == 27:
